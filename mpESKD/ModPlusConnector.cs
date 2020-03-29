69f217bb302851dbfd2b58f9466e65020771d74e
Modis <cjmodis@gmail.com>
2020-03-29 11:20:34 +0300

    AutoCAD 2021

--- mpESKD/ModPlusConnector.cs
+++ mpESKD/ModPlusConnector.cs
@@ -31,6 +31,8 @@ namespace mpESKD
         public string AvailProductExternalVersion => "2019";
 #elif A2020
         public string AvailProductExternalVersion => "2020";
+#elif A2021
+        public string AvailProductExternalVersion => "2021";
 #endif
 
         public string FullClassName => string.Empty;
